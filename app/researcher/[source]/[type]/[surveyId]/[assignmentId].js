1c2b60a0aa9a9bbebc4420926d06e8e3eb0ca659
a-elnawawy <eng.ahmed.elnawawy@gmail.com>
2025-08-17 21:20:18 +0300

    remove pages that use foreign imports

--- app/researcher/[source]/[type]/[surveyId]/[assignmentId].js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-constant-condition */
-import React from 'react';
-import SurveyAssignment from '@components/survey/survey-assignment';
-
-const Survey = () => <SurveyAssignment />;
-
-export default Survey;
