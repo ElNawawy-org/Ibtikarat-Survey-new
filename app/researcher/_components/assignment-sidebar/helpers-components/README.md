f493cd53438cddd5d0f15c5bd72d543b3e4b2a6c
a-elnawawy <eng.ahmed.elnawawy@gmail.com>
2025-08-17 21:14:51 +0300

    remove component that uses foreign imports

--- app/researcher/_components/assignment-sidebar/helpers-components/README.md
+++
@@ -1,2 +0,0 @@
-- All files in this folder 'helpers-components' are temporary and will be deleted during the refactor of the `<AssignmentSidebar />` component
-<!-- TODO-refactor: refactor the `<AssignmentSidebar />` component and all its files -->
---
+++ packages-without-types.d.ts
@@ -0,0 +1,6 @@
+//TODO: -After updating packages to TS- remove this file
+// This file is used to declare modules for packages that do not have type definitions.
+// It allows TypeScript to recognize these packages and avoid type errors.
+
+declare module 'react-gradient-progress';
+declare module 'node-fetch';
